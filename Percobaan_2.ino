caffb3cc1eca2a8f2de2e6e4143d9233b198ad54
Zoni Syahrianto <zoni_syahrianto@yahoo.com>
2016-09-26 21:19:02 +0700

    Add files via upload

---
+++ Percobaan_2.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("ADC Example on A1");
+  analogReference(DEFAULT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned int dataADC = analogRead(A1);
+  lcd.setCursor(0,1);
+  lcd.print("Value = ");
+  lcd.print(dataADC);
+  delay(500);
+  lcd.clear();
+ 
+}
---
+++ Percobaan_4.ino
@@ -0,0 +1,32 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd=(A0,A1,A2,A3,A4,A5);
+boolean comparatorOutput;
+
+ISR (ANALOG_COMP_vect)
+{
+  comparatorOutput = true;
+}
+void setup() 
+{
+  ADCSRB = 0;
+  ACSR = _BV(ACI) | _BV(ACIE) | _BV (ACIS1);
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Analog Comparator");
+}
+
+void loop() 
+{
+  if (comparatorOutput==true)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Comparator High");
+    comparatorOutput = false;
+  }
+  else
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Comparator Low ");
+  }
+}
